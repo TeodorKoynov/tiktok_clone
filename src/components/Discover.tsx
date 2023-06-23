66060eda943e57bbae6602143930e8a884b88a37
TeodorKoynov <teodor.koynov2005@gmail.com>
2023-06-23 14:37:05 +0300

    discover layout

--- src/components/Discover.tsx
+++ src/components/Discover.tsx
@@ -1,6 +1,37 @@
+"use client"
+
+import Link from "next/link";
+import {useSearchParams} from "next/navigation";
+
+import {topics} from "@/utils/constants";
+
 const Discover = () => {
+    const searchParams = useSearchParams()
+    const selectedTopic = searchParams?.get('topic');
+
+    const activeTopicStyle = "xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]";
+    const topicStyle = "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
+
     return (
-        <div>Discover</div>
+        <div className={"xl:border-b-2 xl:border-gray-200 pb-6"}>
+            <p className={"text-gray-500 font-semibold m-3 mt-4 hidden xl:block"}>
+                Popular Topics
+            </p>
+            <div className={"flex gap-3 flex-wrap"}>
+                {topics.map((topic) => (
+                    <Link href={`/?topic=${topic.name}`} key={topic.name}>
+                        <div className={selectedTopic === topic.name ? activeTopicStyle : topicStyle}>
+                            <span className={"font-bold text-2xl xl:text-md"}>
+                                {topic.icon}
+                            </span>
+                            <span className={"font-medium text-md hidden xl:block capitalize"}>
+                                {topic.name}
+                            </span>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
     )
 }
 
