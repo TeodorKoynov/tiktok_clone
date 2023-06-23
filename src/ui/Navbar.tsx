2ede3e08a8ac9204e3e867fb7e89442daf762658
TeodorKoynov <teodor.koynov2005@gmail.com>
2023-06-23 12:22:55 +0300

    initial navbar

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,6 +1,29 @@
+"use client"
+
+import Image from "next/image";
+import Link from "next/link";
+import {useRouter} from "next/router";
+import {GoogleLogin, googleLogout} from "@react-oauth/google";
+
+import {AiOutlineLogout} from "react-icons/ai";
+import {BiSearch} from "react-icons/bi";
+import {IoMdAdd} from "react-icons/io";
+
+import Logo from "../../public/tiktik-logo.png";
+
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className={"w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4"}>
+            <Link href={'/'}>
+                <div className={"w-[100px] md:w-[130px]"}>
+                    <Image
+                        className={"cursor-pointer responsive"}
+                        src={Logo}
+                        alt={'TikTik'}
+                    />
+                </div>
+            </Link>
+        </div>
     )
 }
 
